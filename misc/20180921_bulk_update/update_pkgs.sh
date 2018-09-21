1898d877f4aa263004e83d987faba3e580331d85
charlotte-ngs <ngs.charlotte@gmail.com>
2018-09-21 16:23:50 +0200

    Added installation of rmddochelper pkg

--- misc/20180921_bulk_update/update_pkgs.sh
+++ misc/20180921_bulk_update/update_pkgs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo su - -c "R -e \"pkgs <- installed.packages();ii <- is.na(pkgs[, 'Priority']);pkgs <- pkgs[ii, 1];install.packages(pkgs, repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"devtools::install_github('charlotte-ngs/rmddochelper')\""
